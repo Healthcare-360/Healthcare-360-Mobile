dedb692eabf8c980cfb9d4e30e49b648a6e96aca
Ashley Alex Jacob <alexjacob260@gmail.com>
2022-01-03 12:59:14 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/healthcare360/reliance/healthcare_360_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.healthcare360.reliance.healthcare_360_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/base_view/base_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+
+
+class BaseViewWidget extends StatelessWidget {
+  const BaseViewWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
