ce25c2b75f3f3b5f92174c97c7f4c7262053c707
Ashley Alex Jacob <alexjacob260@gmail.com>
2022-01-12 22:03:50 +0500

    Base View Added

--- lib/ui/base_view/base_view.dart
+++ lib/ui/base_view/base_view.dart
@@ -1,16 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:extended_image/extended_image.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 
+export 'package:get/get.dart';
 
+// export 'package:extended_image/extended_image.dart';
+import 'dart:developer';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:provider/provider.dart';
+export 'package:flutter_svg/flutter_svg.dart';
+export 'package:provider/provider.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:flutter_svg/svg.dart';
 
 class BaseViewWidget extends StatelessWidget {
-  const BaseViewWidget({Key? key}) : super(key: key);
+  final AppBar? appBar;
+  final bool avoidScrollView;
+  final Widget? body;
+  final EdgeInsets? margin;
+  final BottomNavigationBar? bottomNavigationBar;
+  static double height = 1.0;
+  static double width = 1.0;
+  // ignore: prefer_const_constructors_in_immutables
+  BaseViewWidget(
+      {Key? key,
+      this.appBar,
+      this.bottomNavigationBar,
+      required this.avoidScrollView,
+      this.margin,
+      this.body})
+      : super(key: key);
+
+  static devLog(String statement) {
+    log(statement);
+    // ignore: avoid_print
+    print(statement);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // _call(context);
+    height = MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+
+    return appBar != null
+        ? Scaffold(
+            // resizeToAvoidBottomInset: false,
+            bottomNavigationBar: bottomNavigationBar,
+            appBar: appBar,
+            // backgroundColor: MarkaColors.black,
+            body: SafeArea(
+              child: Container(
+                margin: margin ?? EdgeInsets.symmetric(horizontal: 10.w),
+                height: 1.sh,
+                width: 1.sw,
+                child: avoidScrollView
+                    ? body
+                    : CustomScrollView(
+                        slivers: [
+                          SliverFillRemaining(
+                            hasScrollBody: false,
+                            child: body ?? Container(),
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+          )
+        : SafeArea(
+            child: Scaffold(
+              // resizeToAvoidBottomInset: false,
+              bottomNavigationBar: bottomNavigationBar,
+
+              // backgroundColor: MarkaColors.black,
+              body: Container(
+                margin: margin ?? EdgeInsets.symmetric(horizontal: 20.w),
+                height: 1.sh,
+                width: 1.sw,
+                child: avoidScrollView
+                    ? body
+                    : CustomScrollView(
+                        slivers: [
+                          SliverFillRemaining(
+                            hasScrollBody: false,
+                            child: body ?? Container(),
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+          );
   }
 }
